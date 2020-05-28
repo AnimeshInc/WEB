0a543bcfd3acce3c1561cbe939c931df02509da5
AnimeshInc <62781615+AnimeshInc@users.noreply.github.com>
2020-05-28 13:40:12 +0600

    ЛР №5

---
+++ ЛР №5/lab_5/src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Home page</h2>
+        <p>Lorem ipsum dolor sit amet, consect,evptbmmiobe</p>
+        <router-link to="/todos">Todos</router-link>
+    </div>
+</template>
---
+++ ЛР №5/lab_5/src/views/Todos.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+  <h2>Todo application</h2>
+    <router-link to="/todos">Home</router-link>
+  <AddTodo 
+    @add-todo="addTodo"
+  />
+  <select v-model="filter">
+      <option value="all">All</option>
+      <option value="completed">Completed</option>
+      <option value="not-completed">Not Completed</option>
+  </select>
+  <hr>
+  <Loader v-if="loading" />
+  <TodoList 
+  v-else-if="filterTodos.length"
+  v-bind:todos="filterTodos"
+  @remove-todo="removeTodo"
+  />
+  <p v-else>No todos!</p>
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+import Loader from '@/components/Loader'
+export default {
+  name: 'App',
+  data(){
+    return{
+      todos: [],
+      loading: true,
+      filter: 'all'
+    }
+  },
+  mounted(){
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+        this.loading = false
+        setTimeout(() => {
+            this.todos = json
+            this.loading = false
+        }, 2000)
+      })
+  },
+  computed:{
+      filterTodos(){
+          if (this.filter === 'all'){
+              return this.todos
+          }
+          if (this.filter === 'completed'){
+              return this.todos.filter(t => t.completed)
+          }
+          if (this.filter === 'not-completed'){
+              return this.todos.filter(t => !t.completed)
+          }
+      }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    }, 
+    addTodo(todo){
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList, AddTodo, Loader
+  }
+}
+</script>
